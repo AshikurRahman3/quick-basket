a27823dd79afb66ab9c83395d25f8dd7957bfda6
Ashikur Rahman <ashikur.rahman9656@gmail.com>
2025-06-24 23:19:47 +0600

    .

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 
+const dotenv = require('dotenv');
+
+
+// Load environment variables from .env file
+dotenv.config();
+
 app.set('view engine', 'ejs');
-app.use(express.static('public')); 
+app.use(express.static('public'));
 // Add the following line to serve images from the "uploads/shopPhotos" directory
 app.use('/uploads/shopPhotos', express.static('uploads/shopPhotos'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: '123456',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
@@ -27,9 +33,9 @@ const { insertDefaultCategories } = require('./controllers/insert-default-catego
 
 
 app.use(defaultRouter);
-app.use('/api/shop',shopRouter);
-app.use('/customer',customerRouter);
-app.use('/api/bank',bankRouter);
+app.use('/api/shop', shopRouter);
+app.use('/customer', customerRouter);
+app.use('/api/bank', bankRouter);
 
 // connect_database();
 
@@ -38,15 +44,15 @@ app.use('/api/bank',bankRouter);
     await insertDefaultCategories();
 })();
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
 
     res.send('something  broke : ' + err.message);
-    
+
 });
 
-    app.use((req,res,next)=>{
+app.use((req, res, next) => {
 
-        res.send('invalid url');
-        
-    });
+    res.send('invalid url');
+
+});
 module.exports = app;
